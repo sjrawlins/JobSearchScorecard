7353c9dbc0d20c78b4c35002e07c18d132474a20
steverawlins <steverawlins@Steves-MacBook-Pro.local>
2015-07-31 10:38:05 -0500

    Restructure as a result of renaming source to match the git project, namely: JobSearchScorecard instead of just JobScorecard

---
+++ JobSearchScorecard/Model/Steps.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace JobSearchScorecard
+{
+    // Main, top-level categories
+    public enum Steps
+    {
+		All,
+        Attitude,
+        Assessments,
+        MarketingStrategy,
+        MarketingMaterials,
+        Interviewing,
+        FollowUp,
+    }
+    
+}
+
