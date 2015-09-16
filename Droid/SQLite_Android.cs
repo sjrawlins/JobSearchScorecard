eba54681ad8838dc5ea0dc596084bac838a9acec
steverawlins <sjrawlins@gmail.com>
2015-09-16 14:25:05 -0500

    Add Droid components

---
+++ Droid/SQLite_Android.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Xamarin.Forms;
+using JobSearchScorecard;
+using System.IO;
+
+[assembly: Dependency (typeof (SQLite_Android))]
+
+namespace JobSearchScorecard
+{
+	public class SQLite_Android : ISQLite 
+	{
+		public SQLite_Android ()
+		{
+		}
+
+		public SQLite.SQLiteConnection GetConnection () {
+			var sqliteFilename = "JobSearchScorecard.db3";
+			string documentsPath = System.Environment.GetFolderPath (System.Environment.SpecialFolder.Personal); // Documents folder
+			var path = Path.Combine(documentsPath, sqliteFilename);
+			// Create the connection
+			var conn = new SQLite.SQLiteConnection(path);
+			// Return the database connection
+			return conn;
+		}
+	}
+}
+
--- Droid/SplashActivity.cs
+++ Droid/SplashActivity.cs
@@ -2,21 +2,19 @@
 using System.Threading;
 using Android.App;
 using Android.OS;
-using WooddaleScorecard.Droid;
+
 
 namespace SplashScreen
 {
 
-    [Activity (Theme = "@style/Theme.Splash", MainLauncher = true, NoHistory = true)]
-    public class SplashActivity : Activity
-    {
-        protected override void OnCreate (Bundle bundle)
-        {
-            base.OnCreate (bundle);
-            Thread.Sleep (100); // Simulate loading process on app startup (100ms)
-            StartActivity (typeof(MainActivity));
-        }
-    }
+	[Activity (MainLauncher = true, NoHistory = true)]
+	public class SplashActivity : Android.App.Activity
+	{
+		protected override void OnCreate (Bundle bundle)
+		{
+			base.OnCreate (bundle);
+			Thread.Sleep (100); // Simulate loading process on app startup (100ms)
+			StartActivity (typeof(JobSearchScorecard.Droid.MainActivity));
+		}
+	}
 }
-
-
