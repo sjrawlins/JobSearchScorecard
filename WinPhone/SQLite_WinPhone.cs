346b773a379563fac1bc23572bc9f9c7d9eb123a
sjrawlins <sjrawlins@gmail.com>
2015-11-25 16:34:53 -0600

    Got WinPhone working, hurrah!  Use this WinPhone folder, keep the other one for safety only

---
+++ WinPhone/SQLite_WinPhone.cs
@@ -0,0 +1,24 @@
+﻿using JobSearchScorecard;
+using System.IO;
+using Windows.Storage;
+using Xamarin.Forms;
+
+[assembly: Dependency(typeof(SQLite_WinPhone))]
+
+namespace JobSearchScorecard
+{
+
+public class SQLite_WinPhone : ISQLite
+    {
+        public SQLite_WinPhone() { }
+        public SQLite.SQLiteConnection GetConnection()
+        {
+            var sqliteFilename = "JobSearchScorecard.db3";
+            string path = Path.Combine(ApplicationData.Current.LocalFolder.Path, sqliteFilename);
+            // Create the connection
+            var conn = new SQLite.SQLiteConnection(path);
+            // Return the database connection
+            return conn;
+        }
+    }
+}
