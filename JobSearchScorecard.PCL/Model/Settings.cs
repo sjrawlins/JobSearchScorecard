90168602e445dcf571aba91bb7b847d3e84c5bbc
sjrawlins <sjrawlins@gmail.com>
2015-11-29 22:52:40 -0600

    Add a Settings Page
    For email, name and thresholds

---
+++ JobSearchScorecard.PCL/Model/Settings.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using SQLite;
+
+namespace JobSearchScorecard
+{
+	public class Settings
+	{
+		[PrimaryKey, AutoIncrement]
+		public int ID { get; set; } // unique row ID from Database, thus a DB/SQL concept only and not part of the app model
+		public string EmailAddress;
+		public string Name;
+		public int GreenThreshold;
+		public int StarIncrement;
+
+		public Settings ()
+		{
+			Name = string.Empty;
+			EmailAddress = string.Empty;
+			GreenThreshold = 150;  // flip from red to green 
+			StarIncrement = 50;  // gain a new star after each additional
+		}
+	}
+}
+
---
+++ JobSearchScorecard.PCL/Views/SettingsPage.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using System.Diagnostics;
+
+namespace JobSearchScorecard
+{
+	public class SettingsPage : ContentPage
+	{
+		Settings settings;
+		public SettingsPage ()
+		{
+			Title = "Update App Settings";
+
+			settings = App.Database.GetSettings ();
+			if (settings == null)
+				settings = new Settings ();  // 1st time
+
+			var nameEntry = new Entry { Placeholder = "Your name here", };
+			nameEntry.SetBinding (Entry.TextProperty, "Name");
+
+			var emailAddr = new Entry { Keyboard = Keyboard.Email, Placeholder = "your email address here", };
+				//BindingContext = settings.EmailAddress, };
+			emailAddr.SetBinding (Entry.TextProperty, "EmailAddress");
+			
+			var saveButton = new Button { Text = "Save", BorderWidth = 2, };
+			saveButton.Clicked += (sender, e) => {
+				//var settings = (Settings)BindingContext;
+				App.Database.SaveSettings (settings);
+				this.Navigation.PopAsync ();
+			};
+
+			var cancelButton = new Button { Text = "Cancel", BorderWidth = 2, };
+			cancelButton.Clicked += (sender, e) => {
+				this.Navigation.PopAsync ();
+			};
+
+			Content = new StackLayout {
+				Padding = new Thickness (5),
+				Children = {
+					nameEntry,
+					emailAddr,
+					saveButton, 
+					cancelButton,
+				}
+			};
+		}
+	}
+}
+
